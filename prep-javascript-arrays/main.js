877440916b26f53c481550fc81a8943330f99c89
Bryan Jun <bryjun@gmail.com>
2021-09-23 16:14:27 -0400

    commit prep-javascript arrays

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,14 @@
+var languages = ['html', 'css', 'javascript', 'react', 'node', 'postgres'];
+console.log('languages array', languages);
+
+var firstElement = languages[0];
+console.log('1st element of array is', firstElement);
+
+var thirdElement = languages[2];
+console.log('3rd element of array is', thirdElement);
+
+var length = languages.length;
+console.log('length of array is', length);
+
+var lastElement = length - 1;
+console.log('last element is', lastElement);
